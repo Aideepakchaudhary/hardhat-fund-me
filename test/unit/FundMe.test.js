408d588909f5b5510a8d9819bc4e0800c8740f14
Aideepakchaudhary <aideepakchaudhary@gmail.com>
2023-10-20 13:19:33 +0530

    fixing

--- test/unit/FundMe.test.js
+++ test/unit/FundMe.test.js
@@ -1,19 +1,24 @@
 const { deployments, ethers, getNamedAccounts } = require("hardhat"); // runtime environment..
-const await = require("chai");
+const { assert } = require("chai");
 
 describe("FuneMe", async function () {
-    let FundMe;
+    let fundMe;
     let deployer;
     let mockV3Aggregator;
     beforeEach(async function () {
         deployer = (await getNamedAccounts()).deployer;
-        await developmentChains.fixture(["all"]);
+        await deployments.fixture(["all"]);
         fundMe = await ethers.getContract("FundMe", deployer);
-        mockV3Aggregator = await ethers.getContract("MockV3Aggregator");
+        mockV3Aggregator = await ethers.getContract(
+            "MockV3Aggregator",
+            deployer
+        );
     });
 
     describe("constructor", async function () {
-        const response = await fundMe.priceFeed();
-        assert.equal(response, mockV3Aggregator);
+        it("sets the aggregator addresses correctly", async function () {
+            const response = await fundMe.priceFeed();
+            assert.equal(response, mockV3Aggregator.address);
+        });
     });
 });
